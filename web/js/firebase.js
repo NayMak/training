1b3657139303064747fa331ac787b23bfe35fba6
NayMak <nayden.makusa@ext.adeo.com>
2022-02-27 23:31:33 +0100

    init Firebase

---
+++ web/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo2b15JfcA5V64zWv9PNTqpLU3gYasYk4",
+  authDomain: "training-36cf7.firebaseapp.com",
+  projectId: "training-36cf7",
+  storageBucket: "training-36cf7.appspot.com",
+  messagingSenderId: "133332947265",
+  appId: "1:133332947265:web:3de38cac158cfada1a51c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
